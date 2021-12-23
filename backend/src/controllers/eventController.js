84c64699602c3e969e1002b464d34fb9b1ce454e
Kyle Tomanelli <kyletomanelli@gmail.com>
2021-12-23 17:49:18 -0500

    .

--- backend/src/controllers/eventController.js
+++ backend/src/controllers/eventController.js
@@ -16,7 +16,7 @@ class EventController {
         'Content-Type': 'text/event-stream',
         Connection: 'keep-alive',
         'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Origin':'https://kylefrominternet.stream',
+        'Access-Control-Allow-Origin':process.env.BACKEND_URL,
         'Access-Control-Allow-Credentials': true
       };
       res.writeHead(200, headers);
