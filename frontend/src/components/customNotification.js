b674b2b1750455e4bfe4775dce35631ee7a994d0
Kyle Tomanelli <kyletomanelli@gmail.com>
2021-12-29 20:04:39 -0500

    added iframe values

--- frontend/src/components/customNotification.js
+++ frontend/src/components/customNotification.js
@@ -6,9 +6,21 @@ const CustomNotification = (props) => {
     const Iframe = styled.iframe``;
 
     return (
-        <>
-            <Iframe src={`https://ktomanelli.github.io/crtMessage/?message=${props.message}&user=${props.user}`}></Iframe>
-        </>
+        <Iframe 
+            src={`https://ktomanelli.github.io/crtMessage/?message=${props.message}&user=${props.user}`}
+            style={{maxWidth:640, width:'100%', height:'0px', overflow:'visible'}}
+            onLoad={() => {
+                const obj = ReactDOM.findDOMNode(this);
+                this.setState({
+                    "iFrameHeight":  obj.contentWindow.document.body.scrollHeight + 'px'
+                });
+            }} 
+            ref="iframe" 
+            width="100%" 
+            height="0px"
+            scrolling="no" 
+            frameBorder="0"
+        ></Iframe>
     )
 }
 
