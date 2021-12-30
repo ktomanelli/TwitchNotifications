ac7a44a5c4c1f7c938e366c674f7b0433088af0f
Kyle Tomanelli <kyletomanelli@gmail.com>
2021-12-29 20:24:00 -0500

    added width+height for iframe

--- frontend/src/components/customNotification.js
+++ frontend/src/components/customNotification.js
@@ -1,14 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {width, height};
+    }
+  
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+        useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+        }, []);
+    
+        return windowDimensions;
+    }
 
 const CustomNotification = (props) => {
-    useEffect(() => {
-
-    });
+    const { height, width } = useWindowDimensions();
 
     return (
         <iframe 
             src={`https://ktomanelli.github.io/crtMessage/?message=${props.message}&user=${props.user}`}
-            style={{width:'100%', height:'auto'}}
+            style={{width:width, height:height}}
         ></iframe>
     )
 }
