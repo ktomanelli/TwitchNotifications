b8cc6260c2bc1405d771819bfefbad6854ca31e6
Kyle Tomanelli <kyletomanelli@gmail.com>
2021-12-29 20:15:28 -0500

    .

--- frontend/src/components/customNotification.js
+++ frontend/src/components/customNotification.js
@@ -1,26 +1,15 @@
-import styled, { keyframes } from 'styled-components';
-import React from 'react';
-import { StaticImage } from "gatsby-plugin-image"
+import React, {useEffect} from 'react';
 
 const CustomNotification = (props) => {
-    const Iframe = styled.iframe``;
+    useEffect(() => {
+
+    });
 
     return (
-        <Iframe 
+        <iframe 
             src={`https://ktomanelli.github.io/crtMessage/?message=${props.message}&user=${props.user}`}
-            style={{maxWidth:640, width:'100%', height:'0px', overflow:'visible'}}
-            onLoad={() => {
-                const obj = ReactDOM.findDOMNode(this);
-                this.setState({
-                    "iFrameHeight":  obj.contentWindow.document.body.scrollHeight + 'px'
-                });
-            }} 
-            ref="iframe" 
-            width="100%" 
-            height="0px"
-            scrolling="no" 
-            frameBorder="0"
-        ></Iframe>
+            style={{width:'100%', height:'auto'}}
+        ></iframe>
     )
 }
 
